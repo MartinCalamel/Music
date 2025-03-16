0895e4d64b62ead5ff87d208655b554993e6f5ea
MartinCalamel <martin.calamel@cpe.fr>
2025-03-16 02:02:14 +0100

    bot done

--- Code/fonction.py
+++ Code/fonction.py
@@ -11,6 +11,7 @@ import requests
 import re
 import json
 import yt_dlp
+import os
 
 
 
@@ -50,10 +51,16 @@ def extract_data_from_soup(soup):
     else:
         print("Variable ytInitialData non trouvée.")
 
-def make_choice(video):
+def make_choice(video,bot = False):
     """
     fonction pour proposer les choix de video retourne l'id de la video choisie
     """
+    if bot:
+        msg = ""
+        msg += "choisissez la video que vous voulez télécharger\n"
+        for i in range(5):
+            msg += f'[ {i} ] {video[i][1]}\n'
+        return msg
     print("choisissez la video que vous voulez télécharger")
     for i in range(5):
         print(f'[ {i} ] {video[i][1]}')
@@ -62,16 +69,42 @@ def make_choice(video):
 def make_url(id):
     return f'https://youtu.be/{id}'
 
+# def download(url):
+#     ydl_opts = {
+#         'format': 'm4a/bestaudio',  # Télécharge l'audio au format M4A
+#         'outtmpl': './music/%(title)s.%(ext)s',  # Nom du fichier = titre de la vidéo
+#     }
+
+#     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+#         ydl.download([url])
+
+
 def download(url):
-    ydl_opts = {
-        'format': 'm4a/bestaudio',  # Télécharge l'audio au format M4A
-        'outtmpl': './music/%(title)s.%(ext)s',  # Nom du fichier = titre de la vidéo
-    }
+    def clean_title(title):
+        # Remplacer les caractères non valides par '-'
+        return re.sub(r'[⧸/\\]', '-', title)
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+    with yt_dlp.YoutubeDL() as ydl:
+        info_dict = ydl.extract_info(url, download=False)
+        video_title = info_dict.get('title', None)
+        video_extension = "m4a"
+        
+        if video_title:
+            # Nettoyer le titre avant de définir le nom du fichier
+            safe_title = clean_title(video_title)
+            ydl_opts = {
+                'format': 'm4a/bestaudio',  # Télécharge l'audio au format M4A
+                'outtmpl': f'./music/{safe_title}.%(ext)s',  # Utilisation du titre nettoyé
+            }
+            
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+            
+            new_file_name = f'./music/{safe_title}.{video_extension}'
+            return new_file_name
+        else:
+            return None
 
-    print("Téléchargement terminé !")
 
 def main():
     query = input("Entrez le terme de recherche (ex: 'Python tutorial') : ")
