ff2276b495e4d04b5e9804ba9360ac7a1e5c2d73
MartinCalamel <martin.calamel@cpe.fr>
2025-03-17 18:28:16 +0100

    try docker the app

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python
+
+
+WORKDIR /app
+
+
+COPY requirement.txt requirement.txt
+
+RUN pip install -r requirement.txt
+
+COPY . .
+
+CMD ["python", "bot.py" ]
+
--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-python3 -m venv .venv
-source .venv/bin/activate
-pip install -r requirement.txt
-mv Code/install_need/startup.service ~/.config/systemd/user/startup.service
-systemctl --user enable startup.service
-systemctl --user start startup.service
